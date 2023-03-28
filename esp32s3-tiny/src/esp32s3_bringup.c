df592da900774bd208e6a93773df804415c53a0a
Huang Qi <huangqi3@xiaomi.com>
2023-03-28 16:52:18 +0800

    Initial support for esp32s3-tiny

--- esp32s3-tiny/src/esp32s3_bringup.c
+++ esp32s3-tiny/src/esp32s3_bringup.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * boards/xtensa/esp32/esp32-devkitc/src/esp32_bringup.c
+ * boards/xtensa/esp32s3/esp32s3-eye/src/esp32s3_bringup.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -35,130 +35,31 @@
 #include <debug.h>
 #include <stdio.h>
 
-#include <arch/board/board.h>
-
 #include <errno.h>
-#if defined(CONFIG_ESP32_EFUSE)
-#include <nuttx/efuse/efuse.h>
-#endif
 #include <nuttx/fs/fs.h>
-#include <nuttx/himem/himem.h>
-#include <nuttx/board.h>
-#include <nuttx/lcd/lcd_dev.h>
 
-#if defined(CONFIG_ESP32_EFUSE)
-#include "esp32_efuse.h"
-#endif
-#include "esp32_partition.h"
-
-#ifdef CONFIG_USERLED
-#  include <nuttx/leds/userled.h>
+#ifdef CONFIG_ESP32S3_TIMER
+#  include "esp32s3_board_tim.h"
 #endif
 
-#ifdef CONFIG_CAN_MCP2515
-#  include "esp32_mcp2515.h"
-#endif
-
-#ifdef CONFIG_TIMER
-#include <esp32_tim_lowerhalf.h>
-#endif
-
-#ifdef CONFIG_ONESHOT
-#  include "esp32_board_oneshot.h"
+#ifdef CONFIG_ESP32S3_RT_TIMER
+#  include "esp32s3_rt_timer.h"
 #endif
 
 #ifdef CONFIG_WATCHDOG
-#  include "esp32_board_wdt.h"
-#endif
-
-#ifdef CONFIG_ESP32_SPIFLASH
-#  include "esp32_board_spiflash.h"
-#endif
-
-#ifdef CONFIG_ESP32_BLE
-#  include "esp32_ble.h"
-#endif
-
-#ifdef CONFIG_ESP32_WIFI
-#  include "esp32_board_wlan.h"
-#endif
-
-#ifdef CONFIG_ESP32_WIFI_BT_COEXIST
-#  include "esp32_wifi_adapter.h"
-#endif
-
-#ifdef CONFIG_ESP32_I2C
-#  include "esp32_board_i2c.h"
-#endif
-
-#ifdef CONFIG_ESP32_I2S
-#  include "esp32_i2s.h"
-#endif
-
-#ifdef CONFIG_ESP32_PCNT_AS_QE
-#  include "board_qencoder.h"
-#endif
-
-#ifdef CONFIG_I2CMULTIPLEXER_TCA9548A
-#  include "esp32_tca9548a.h"
-#endif
-
-#ifdef CONFIG_SENSORS_BMP180
-#  include "esp32_bmp180.h"
-#endif
-
-#ifdef CONFIG_SENSORS_BMP280
-#  include "esp32_bmp280.h"
-#endif
-
-#ifdef CONFIG_SENSORS_SHT3X
-#  include "esp32_sht3x.h"
-#endif
-
-#ifdef CONFIG_SENSORS_MS5611
-#  include "esp32_ms5611.h"
-#endif
-
-#ifdef CONFIG_LCD_HT16K33
-#  include "esp32_ht16k33.h"
-#endif
-
-#ifdef CONFIG_ESP32_AES_ACCELERATOR
-#  include "esp32_aes.h"
-#endif
-
-#ifdef CONFIG_ESP32_RT_TIMER
-#  include "esp32_rt_timer.h"
+#  include "esp32s3_board_wdt.h"
 #endif
 
 #ifdef CONFIG_INPUT_BUTTONS
 #  include <nuttx/input/buttons.h>
 #endif
 
-#ifdef CONFIG_RTC_DRIVER
-#  include "esp32_rtc_lowerhalf.h"
-#endif
-
-#ifdef CONFIG_SPI_DRIVER
-#  include "esp32_spi.h"
-#endif
-
-#ifdef CONFIG_LCD_BACKPACK
-#  include "esp32_lcd_backpack.h"
-#endif
-
-#ifdef CONFIG_SENSORS_MAX6675
-#  include "esp32_max6675.h"
-#endif
-
-#include "esp32_gpio.h"
-
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: esp32_bringup
+ * Name: esp32s3_bringup
  *
  * Description:
  *   Perform architecture-specific initialization
@@ -171,18 +72,9 @@
  *
  ****************************************************************************/
 
-int esp32_bringup(void)
+int esp32s3_bringup(void)
 {
   int ret;
-
-#ifdef CONFIG_ESP32_AES_ACCELERATOR
-  ret = esp32_aes_init();
-  if (ret < 0)
-    {
-      syslog(LOG_ERR,
-             "ERROR: Failed to initialize AES: %d\n", ret);
-    }
-#endif
 
 #ifdef CONFIG_FS_PROCFS
   /* Mount the procfs file system */
@@ -193,7 +85,6 @@ int esp32_bringup(void)
       syslog(LOG_ERR, "ERROR: Failed to mount procfs at /proc: %d\n", ret);
     }
 #endif
-
 
 #ifdef CONFIG_FS_TMPFS
   /* Mount the tmpfs file system */
@@ -206,38 +97,56 @@ int esp32_bringup(void)
     }
 #endif
 
-#ifdef CONFIG_MMCSD
-  ret = esp32_mmcsd_initialize(0);
+#ifdef CONFIG_ESP32S3_TIMER
+  /* Configure general purpose timers */
+
+  ret = board_tim_init();
   if (ret < 0)
     {
-      syslog(LOG_ERR, "Failed to initialize SD slot: %d\n", ret);
+      syslog(LOG_ERR, "Failed to initialize timers: %d\n", ret);
     }
 #endif
 
-#ifdef CONFIG_ESP32_RT_TIMER
-  ret = esp32_rt_timer_init();
+#ifdef CONFIG_ESP32S3_RT_TIMER
+  ret = esp32s3_rt_timer_init();
   if (ret < 0)
     {
       syslog(LOG_ERR, "Failed to initialize RT timer: %d\n", ret);
     }
 #endif
 
-#ifdef CONFIG_RTC_DRIVER
-  /* Instantiate the ESP32 RTC driver */
+#ifdef CONFIG_WATCHDOG
+  /* Configure watchdog timer */
 
-  ret = esp32_rtc_driverinit();
+  ret = board_wdt_init();
   if (ret < 0)
     {
-      syslog(LOG_ERR,
-             "ERROR: Failed to Instantiate the RTC driver: %d\n", ret);
+      syslog(LOG_ERR, "Failed to initialize watchdog timer: %d\n", ret);
     }
 #endif
 
-  board_lcd_initialize();
+#ifdef CONFIG_INPUT_BUTTONS
+  /* Register the BUTTON driver */
 
-  lcddev_register(0);
+  ret = btn_lower_initialize("/dev/buttons");
+  if (ret < 0)
+    {
+      syslog(LOG_ERR, "Failed to initialize button driver: %d\n", ret);
+    }
+#endif
 
-  esp32_gpiowrite(DISPLAY_BCKL, false);
+#ifdef CONFIG_ESP32S3_SPIFLASH
+  ret = board_spiflash_init();
+  if (ret)
+    {
+      syslog(LOG_ERR, "ERROR: Failed to initialize SPI Flash\n");
+    }
+#endif
+
+  /* If we got here then perhaps not all initialization was successful, but
+   * at least enough succeeded to bring-up NSH with perhaps reduced
+   * capabilities.
+   */
 
   UNUSED(ret);
   return OK;
